144a767d97cb7dcc138f1fe700107d01d46e1c4c
wanda belingga putra <45660041+WandaBelinggaPutra@users.noreply.github.com>
2019-01-04 20:48:44 +0700

    Add files via upload

---
+++ bilangan terbesar dari dua bilangan.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a,b;
+
+cout<<"menentukan bilangan terbesar dari 2 buah bilangan"<<endl;
+cout<<"masukan bilangan ke 1"<<endl;
+cin>>a;
+cout<<"masukan bilangan ke 2"<<endl;
+cin>>b;
+
+if(a>b)
+	cout<<"maka bilangan terbesar adalah"<<a<<endl;
+	else
+		cout<<"maka bilangan terbesar adalah"<<b;
+	return 0;
+}
